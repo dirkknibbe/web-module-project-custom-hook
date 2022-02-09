50f00e34b78462dc42171ed9e0ada1c026cd9be3
Dirk Knibbe <dirkknibbe@Dirks-MacBook-Pro.local>
2022-02-09 14:23:14 -0700

    ok finished the steps in the readme and it looks like the darkmode persists in local storage

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import useLocalStorage from "./useLocalStorage";
+
+const useDarkMode = () => {
+  const [darkMode, setDarkMode] = useLocalStorage();
+  return [darkMode, setDarkMode];
+};
+
+export default useDarkMode;
---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+const useLocalStorage = (key, initialValue) => {
+  const setIt = (val) => window.localStorage.setItem(key, JSON.stringify(val));
+  const [value, setValue] = useState(() => {
+    const item = window.localStorage.getItem(key);
+    if (item) {
+      return JSON.parse(item);
+    }
+    setIt(initialValue);
+    return initialValue;
+  });
+  const setValueLS = (newValue) => {
+    setIt(newValue);
+    setValue(newValue);
+  };
+
+  return [value, setValueLS];
+};
+
+export default useLocalStorage;
